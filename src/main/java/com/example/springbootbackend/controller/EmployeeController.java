e5f9604d73e5867b0a1d74f2b18a09c3bcb9d072
Sohanur Islam <shawon411184@gmail.com>
2021-11-04 11:36:00 +0600

    add cross origin annotation for access Api

--- src/main/java/com/example/springbootbackend/controller/EmployeeController.java
+++ src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -3,11 +3,13 @@ package com.example.springbootbackend.controller;
 import com.example.springbootbackend.model.Employee;
 import com.example.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class EmployeeController {
     @Autowired
